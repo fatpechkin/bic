2552091d6db88140d0af53f3015492d9a165257e
Sergey Postnikov <sergey_postnikov@epam.com>
2022-04-21 16:51:19 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,155 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String inputFile = "lng.csv";
+        String outputFile = "output.txt";
+        List<String> lines = readInput(inputFile).stream().toList();
+        if (lines == null) {
+            return;
+        }
+        lines = prepareStrings(lines);
+        List<List<String>> result = processLines(lines);
+        result = result.stream()
+                .filter(l -> l.size() > 1)
+                .sorted((o1, o2) -> o2.size() - o1.size())
+                .collect(Collectors.toList());
+        writeOutput(outputFile, resultToString(result));
+    }
+
+    private static class Dot {
+        private final String val;
+        private final int index;
+
+        public Dot(String val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+    }
+    private static HashSet<String> readInput(String inputFile) {
+        HashSet<String> localLines = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            String line = br.readLine();
+            while (line != null) {
+                localLines.add(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return localLines;
+    }
+
+    private static List<String> prepareStrings(List<String> lines) {
+        lines = lines.stream()
+                .parallel()
+                .map(line -> line.split(";"))
+                .map(arr ->
+                        Arrays.stream(arr)
+                                .map(str -> {
+                                    if (str.charAt(0) == '\"') {
+                                        str = str.substring(1, str.length() - 1);
+                                    }
+                                    if (str.length() == 0) {
+                                        return "";
+                                    }
+                                    if (str.charAt(str.length() - 1) == '\"') {
+                                        str = str.substring(0, str.length() - 2);
+                                    }
+                                    if (str.contains("\"")) {
+                                        return "";
+                                    }
+                                    return str;
+                                })
+                                .filter(str -> str.length() > 0)
+                                .collect(Collectors.joining(";")))
+                .filter(line -> line.length() > 0)
+                .toList();
+        return lines;
+    }
+
+    private static List<List<String>> processLines(List<String> lines) {
+
+        List<List<String>> groups = new ArrayList<>();
+        if (lines.size() < 2) {
+            groups.add(lines);
+            return groups;
+        }
+
+        List<Map<String, Integer>> dotMap = new ArrayList<>();
+        Map<Integer, Integer> crossMap = new HashMap<>();
+        for (String line : lines) {
+            String[] lineDots = line.split(";");
+            TreeSet<Integer> matchingGroups = new TreeSet<>();
+            List<Dot> dots = new ArrayList<>();
+
+            for (int i = 0; i < lineDots.length; i++) {
+                String dot = lineDots[i];
+                if (dotMap.size() == i)
+                    dotMap.add(new HashMap<>());
+
+                Map<String, Integer> currentIndex = dotMap.get(i);
+                Integer dotGroupNumber = currentIndex.get(dot);
+                if (dotGroupNumber != null) {
+                    while (crossMap.containsKey(dotGroupNumber))
+                        dotGroupNumber = crossMap.get(dotGroupNumber);
+                    matchingGroups.add(dotGroupNumber);
+                } else {
+                    dots.add(new Dot(dot, i));
+                }
+            }
+            int groupNumber;
+            if (matchingGroups.isEmpty()) {
+                groups.add(new ArrayList<>());
+                groupNumber = groups.size() - 1;
+            } else {
+                groupNumber = matchingGroups.first();
+            }
+            for (Dot dot : dots) {
+                dotMap.get(dot.index).put(dot.val, groupNumber);
+            }
+            for (int matchedGrNum : matchingGroups) {
+                if (matchedGrNum != groupNumber) {
+                    crossMap.put(matchedGrNum, groupNumber);
+                    groups.get(groupNumber).addAll(groups.get(matchedGrNum));
+                    groups.set(matchedGrNum, null);
+                }
+            }
+            groups.get(groupNumber).add(line);
+        }
+        groups.removeAll(Collections.singleton(null));
+        return groups;
+    }
+
+    private static String resultToString(List<List<String>> result) {
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        for (List<String> list :
+                result) {
+            if (list.size() > 1) {
+                sb.append("Group " + i + "\n");
+                for (String s :
+                        list) {
+                    sb.append(String.join(";", s) + "\n");
+                }
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+
+    private static void writeOutput(String outputFile, String content) {
+        try (PrintWriter writer = new PrintWriter(outputFile)) {
+            writer.println(content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
